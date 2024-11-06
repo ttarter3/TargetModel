93e74f334fc73a84f1ae243cb2d19b0ee9329f3e
ttarter3 <ttarter3@gmail.com>
2024-11-05 22:08:13 -0600

    Major Refactor Broken Feature Branch

--- include/TargetList.py
+++ include/TargetList.py
@@ -15,7 +15,10 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
+import numpy as np
 
+from config.Config import Config
+from partyfirst.targetmodel.include.Model.Model3D import Model3D
 from partyfirst.targetmodel.include.Target import Target
 
 
@@ -29,3 +32,25 @@ class TargetList():
     if isinstance(target, TargetList):
       self.targets_.extend(target.targets_)
     else: self.targets_.append(target)
+
+  def CalcTargetPointingAngle(self, pos_ecef_xyz_m):
+    # NOTE: Point at the first target, not always the case
+    med = np.median(self.targets_[0].tsp_.pos_ecf_3m_, axis=0)
+    pointing_vector = med - pos_ecef_xyz_m
+    point = pointing_vector / np.linalg.norm(pointing_vector)
+    return point
+
+  @staticmethod
+  def Load():
+    if Config.config("target_set")["type"] == "uav" and Config.config("target_set")["relative_path"] is None:
+      from partyfirst.targetmodel.include.TSP.UASAttackFile import UASAttackFile
+
+      tgt_list = TargetList()
+      sub_tgt_list = UASAttackFile.LoadTargetData()
+      for tsp in sub_tgt_list:
+        rcs = Model3D.GetModel3D(tsp.object_name_)
+        tgt = Target(tsp, rcs)
+        tgt_list.AddTarget(tgt)
+      return tgt_list
+
+    else: raise ValueError("Unsupported Target Set")
